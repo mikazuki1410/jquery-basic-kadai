9bbee7ae70890e34d1f1c4cb49739542cc86b0ab
mikazuki1410 <h-250814291010.mk@outlook.jp>
2024-09-02 23:37:40 +0900

    kadai_004

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+
+  //loadイベントが発生したとき
+  $(window).on('load', function() {
+    console.log('loadイベントが発生しました');
+  });
+  
+  //scrollイベントが発生したとき
+  $(window).on('scroll', function() {
+    console.log('scrollイベントが発生しました');
+  });
